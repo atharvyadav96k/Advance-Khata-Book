b75e7b5e4f0b5aced44f50ab7b36f370b5da7b1f
atharvyadav96k <atharvyadav96k@gmail.com>
2024-06-20 15:12:31 +0530

    updated register route and removed isAuthencated middelware

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 const express = require('express')
 const app = express();
+const userSchema = require("./routes/login")
 
-app.get('/', function(req, res){
-    res.send("Hello")
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get('/',function(req, res){
+    res.render('index')
+});
+app.get('/login', function(req, res){
+    res.render('login')
+});
+app.post('/login', async function(req, res){
+    try{
+        const user = await userSchema.findOne({email: req.body.email});
+        if(user.password == req.body.password){
+            res.send("login Successful")
+        }else{
+            res.send("login failed")
+        }
+    }catch(err){
+        res.send(err)
+    }
+});
+app.get('/register', function(req, res){
+    res.render('register')
+});
+app.post("/register", async function(req, res){
+    try{
+        const user = await userSchema({
+            email : req.body.email,
+            password: req.body.password
+        })
+        await user.save();
+        res.send(user)
+    }catch(err){
+        res.send(err)
+    }
+    
+});
 
 app.listen(3000);
--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose')
 mongoose.connect("mongodb://localhost:27017/khaatabook");
 
 const userSchema = mongoose.Schema({
-    email: String,
-    password: String
+    email: {
+        type : String,
+        unique: true,
+        required: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    }
 })
 module.exports = mongoose.model("user", userSchema);
